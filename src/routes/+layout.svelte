a7dbdd225e6daeb23165d3cd9f077be9c2c57517
Larissa <larissa.mikkers@hotmail.com>
2024-11-07 13:30:58 +0100

    header + footer toegevoegd
    Beginnetje van nav, header en footer gemaakt, global styles toegevoegd (nog niet alles)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,50 @@
 <!-- Navigation -->
+ <nav>
+    <section>
+        <a href="/"><img src="" alt="Logo van Larissa Mikkers"></a>
+        <p>Amsterdam | 14:00 | 8°C</p>
+    </section>
+
+    <ul>
+        <li><a aria-label="projecten" href="/">Projecten</a></li>
+        <li><a aria-label="blog i love web" href="/">i-love-web</a></li>
+        <li><a aria-label="contact" href="/">Contact</a></li>
+    </ul>
+ </nav>
 
 <slot></slot>
 
 <!-- Footer -->
+<footer>
+    <ul>
+        <li><a href="/contact">Contact</a></li>
+        <li>©2024 Larissa Mikkers</li>
+        <li><a href="/sitemap">Sitemap</a></li>
+    </ul>
+</footer>
+
+<style>
+    nav {
+        width: 100vw  ;
+        position: fixed;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    footer {
+        width: 100%;
+        height: 4rem;
+        background-color: rgb(239, 239, 239);
+        display: flex;
+        justify-content: center;
+    }
+    nav ul,
+    footer ul {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        gap: 1rem;
+        list-style: none;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Larissa Mikkers</h1>
-<p>Frontend Developer</p>
+<script>
+
+</script>
+
+<header>
+    <article>
+        <h1>Larissa Mikkers</h1>
+        <h2>Frontend Design and Development</h2>
+    </article>
+</header>
+
+<main>
+
+</main>
+
+<style>
+    header {
+        width: 100vw;
+        height: 100vh;
+        background-color: var(--mainColor);
+        color: var(--secondColor);
+        text-align: center;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    header h1 {
+        font-size: 3.5rem;
+        text-transform: uppercase;
+    }
+    header h2 {
+        font-family: var(--normalFont);
+    }
+
+</style>
